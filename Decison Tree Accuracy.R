282a23b751a54aee234ed2d79c0b31d7f6448e3d
Ali <alikiyand@gmail.com>
2017-10-20 22:46:20 +0100

    calculating accuracy for decision tree

--- Decison Tree Accuracy.R
+++ Decison Tree Accuracy.R
@@ -3,6 +3,9 @@ occupancy_test_f <- occupancy_test[,c(2, 3, 4, 5, 7)]
 occupancy_test_f$Occupancyf <- as.factor(occupancy_test_f$Occupancy)
 library(party)
 occupancy_tree <- ctree(Occupancyf~Temperature+Humidity+Light+CO2, occupancy_test_f)
+validate <- read.table("datatest.txt", sep=",")
+validate <- validate[,c(2, 3, 4, 5, 7)]
+validate$Occupancyf <- as.factor(validate$Occupancy)
 test_predict <- table(predict(occupancy_tree, newdata= validate), validate$Occupancyf) 
 accuracy <- sum(diag(test_predict))/sum(test_predict)
 
